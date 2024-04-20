757b70c726ad135a486d5c85924c73c7ffa2242a
Saranya Govindaraj <findsaranyag@gmail.com>
2024-04-20 22:16:21 +0530

    difference btwn JSX and React create

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
- import  React  from "react";
- import  ReactDOM  from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
- //React.createElement => Object => when rendering on the dom it's converted into HTML Element
+//React.createElement => Object => when rendering on the dom it's converted into HTML Element
 
- const heading = React.createElement("h1",{id:'heading'},"Hello React 🚀 ");
- const h1 = <h1 id="title">JSX hello 🌹 </h1>;
- console.log(h1);
- console.log(heading);
+const heading = React.createElement(
+  "h1",
+  { id: "heading", className: "title" },
+  "Hello React 🚀 "
+);
+const h1 = (
+  <h1 id="title" className="title">
+    JSX hello 🌹{" "}
+  </h1>
+);
 
- const root = ReactDOM.createRoot(document.getElementById('root'));
- root.render(h1);
+console.log(h1);
+console.log(heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
