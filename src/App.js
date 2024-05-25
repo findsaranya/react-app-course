757e050764d6713384e8bb6b3034e0bb0a5734ee
Saranya Govindaraj <findsaranyag@gmail.com>
2024-05-25 14:40:22 +0530

    react hooks with mock datas

--- src/App.js
+++ src/App.js
@@ -3,63 +3,6 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 
-const restData = {
-  id: "582150",
-  name: "Chinese Wok",
-  cloudinaryImageId: "e0839ff574213e6f35b3899ebf1fc597",
-  locality: "Avinashi Road",
-  areaName: "Peelamedu",
-  costForTwo: "₹250 for two",
-  cuisines: ["Chinese", "Asian", "Tibetan", "Desserts"],
-  avgRating: 4.1,
-  parentId: "61955",
-  avgRatingString: "4.1",
-  totalRatingsString: "1K+",
-  sla: {
-    deliveryTime: 29,
-    lastMileTravel: 3.9,
-    serviceability: "SERVICEABLE",
-    slaString: "25-30 mins",
-    lastMileTravelString: "3.9 km",
-    iconType: "ICON_TYPE_EMPTY",
-  },
-  availability: {
-    nextCloseTime: "2024-04-29 02:00:00",
-    opened: true,
-  },
-  badges: {},
-  isOpen: true,
-  type: "F",
-  badgesV2: {
-    entityBadges: {
-      imageBased: {},
-      textBased: {},
-      textExtendedBadges: {},
-    },
-  },
-  aggregatedDiscountInfoV3: {
-    header: "ITEMS",
-    subHeader: "AT ₹169",
-  },
-  orderabilityCommunication: {
-    title: {},
-    subTitle: {},
-    message: {},
-    customIcon: {},
-  },
-  differentiatedUi: {
-    displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
-    differentiatedUiMediaDetails: {
-      mediaType: "ADS_MEDIA_ENUM_IMAGE",
-      lottie: {},
-      video: {},
-    },
-  },
-  reviewsSummary: {},
-  displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
-  restaurantOfferPresentationInfo: {},
-};
-
 const AppLayout = () => (
   <div className="app-layout">
     <Header />
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,20 +1,75 @@
 import RestuarantCard from "./RestuarantCard";
 import { data } from "../utils/mockData";
+import { useState } from "react";
 
 const { data: cardData } = data;
 const { cards } = cardData;
+const restaurantList = [
+  {
+    info: {
+      id: "52788",
+      name: "Sree Ram Mess",
+      cloudinaryImageId: "lk9qcvyopdpyiep3pov0",
+      costForTwo: "₹200 for two",
+      cuisines: ["South Indian"],
+      avgRating: 4.5,
+      sla: {
+        deliveryTime: 23,
+      },
+    },
+  },
+  {
+    info: {
+      id: "52789",
+      name: "Sree Ram Mess One",
+      cloudinaryImageId: "lk9qcvyopdpyiep3pov0",
+      costForTwo: "₹200 for two",
+      cuisines: ["South Indian"],
+      avgRating: 3.9,
+      sla: {
+        deliveryTime: 23,
+      },
+    },
+  },
+  {
+    info: {
+      id: "52790",
+      name: "Sree Ram Mess One",
+      cloudinaryImageId: "lk9qcvyopdpyiep3pov0",
+      costForTwo: "₹200 for two",
+      cuisines: ["South Indian"],
+      avgRating: 4,
+      sla: {
+        deliveryTime: 23,
+      },
+    },
+  },
+];
 
-export default Body = () => {
-  let cardsList = cards.map((eachCard) => (
-    <RestaurantCard
-      restData={eachCard.card.card.info}
-      key={eachCard.card.card.info.id}
-    />
+export const Body = () => {
+  const [listOfRestaurant, setFilterRest] = useState([...restaurantList]);
+  let cardsList = listOfRestaurant.map((eachCard) => (
+    <RestaurantCard restData={eachCard.info} key={eachCard.info.id} />
   ));
   return (
     <div className="body">
       <div className="search">SearchBar</div>
+      <div className="filter">
+        <button
+          onClick={() => {
+            const restaurantList = listOfRestaurant.filter(
+              (res) => res.info.avgRating >= 4
+            );
+            setFilterRest([...restaurantList]);
+            // console.log(restaurantList);
+          }}
+        >
+          click top rated🍓
+        </button>
+      </div>
       <div className="res-container">{cardsList}</div>
     </div>
   );
 };
+
+export default Body;
